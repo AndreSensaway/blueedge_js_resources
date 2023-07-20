9446e9ed1736c279b372783e30e0d4b467c6479d
AndreSensaway <42672191+AndreSensaway@users.noreply.github.com>
2023-07-20 14:31:56 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
-export function dateStringToTimestamp(dateString) {
-  const timestamp = Date.parse(dateString);
-  if (isNaN(timestamp)) {
-    return null;
-  }
-  return timestamp;
+// Define the library as an object (namespaced to avoid global pollution)
+var MyMathLibrary = MyMathLibrary || {};
+
+// Add a function to the library
+MyMathLibrary.addNumbers = function (num1, num2) {
+  return num1 + num2;
+};
+
+// Export the library for use in CommonJS environments (Node.js)
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = MyMathLibrary;
 }
